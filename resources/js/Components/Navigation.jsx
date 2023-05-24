d082338eb6a62f8cf83936f4cc4564aaa44c0d31
richardhaddadau <richard@haddads.net.au>
2023-05-24 19:10:07 +1000

    build: navigation bar fleshing out

--- resources/js/Components/Navigation.jsx
+++ resources/js/Components/Navigation.jsx
@@ -1,11 +1,53 @@
+import NavLogo from "./NavLogo.jsx";
+
 const Navigation = () => {
+  const navItems = [
+    {
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "Packages",
+      url: "/packages",
+    },
+    {
+      title: "Services",
+      url: "/services",
+    },
+    {
+      title: "Projects",
+      url: "/projects",
+    },
+    {
+      title: "Contact",
+      url: "/contact",
+    },
+  ];
   return (
-    <nav className="px-14 py-8 grid grid-cols-12 w-full bg-black text-white">
-      <section className="col-span-2">Logo Here</section>
-      <section className="col-span-8 flex justify-center">
-        Navigation Here
+    <nav className="px-6 lg:px-14 py-8 grid grid-cols-12 w-full bg-black text-white">
+      <section className="col-span-2 items-center">
+        <NavLogo className="h-6 lg:h-8 text-red-500" />
       </section>
-      <section className="col-span-2 flex justify-end">CTA Here</section>
+      <section className="col-span-8 gap-10 flex justify-center items-center uppercase text-sm">
+        {navItems.map((nav) => {
+          return (
+            <>
+              <a
+                href={nav.url}
+                title={nav.title}
+                className="hover:text-yellow-500 transition-all duration-200"
+              >
+                {nav.title}
+              </a>
+            </>
+          );
+        })}
+      </section>
+      <section className="col-span-2 flex justify-end items-center">
+        <button className="px-4 py-1 border-2 border-white hover:border-yellow-500 hover:bg-yellow-500 rounded-full uppercase text-sm hover:text-black font-normal transition-all duration-200">
+          Build with us
+        </button>
+      </section>
     </nav>
   );
 };
