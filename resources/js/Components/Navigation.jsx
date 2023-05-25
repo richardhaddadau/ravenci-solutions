fb91b8bc3c09b325e4f193c2a5d500bc06e21ea7
richardhaddadau <richard@haddads.net.au>
2023-05-25 21:50:48 +1000

    build: previous work fleshed out - static

--- resources/js/Components/Navigation.jsx
+++ resources/js/Components/Navigation.jsx
@@ -1,6 +1,9 @@
 import NavLogo from "./NavLogo.jsx";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const navItems = [
     {
       title: "Home",
@@ -23,30 +26,64 @@ const Navigation = () => {
       url: "/contact",
     },
   ];
+
+  const handleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className="px-6 lg:px-14 py-8 grid grid-cols-12 w-full bg-black text-white">
+    <nav className="px-6 lg:px-14 py-8 flex lg:grid grid-cols-12 justify-between items-center w-full bg-black text-white">
       <section className="col-span-2 items-center">
         <NavLogo className="h-6 lg:h-8 text-red-500" />
       </section>
-      <section className="col-span-8 gap-10 flex justify-center items-center uppercase text-sm">
-        {navItems.map((nav) => {
-          return (
-            <>
-              <a
-                href={nav.url}
-                title={nav.title}
-                className="font-abril hover:text-yellow-500 font-extrabold transition-all duration-200"
-              >
-                {nav.title}
-              </a>
-            </>
-          );
-        })}
+
+      <section className="lg:hidden flex justify-end z-50">
+        <div
+          className="cursor-pointer relative pt-2.5 pb-3 px-2 flex flex-col justify-between w-11 h-11"
+          onClick={handleMenu}
+        >
+          <span
+            className={
+              (menuOpen ? "rotate-45 translate-y-3" : null) +
+              " block h-0.5 bg-white transform transition-all duration-500"
+            }
+          ></span>
+          <span
+            className={
+              (menuOpen ? "opacity-0" : null) +
+              " block h-0.5 bg-white transition-all duration-500"
+            }
+          ></span>
+          <span
+            className={
+              (menuOpen ? "-rotate-45 -translate-y-1.5" : null) +
+              " block h-0.5 bg-white transition-all duration-500"
+            }
+          ></span>
+        </div>
       </section>
-      <section className="col-span-2 flex justify-end items-center">
-        <button className="px-4 pt-2 pb-1 border-2 border-white hover:border-yellow-500 hover:bg-yellow-500 rounded-full uppercase font-abril text-sm hover:text-black transition-all duration-200">
-          Build with us
-        </button>
+
+      <section className="hidden lg:grid grid-cols-10 col-span-10">
+        <section className="col-span-8 gap-10 flex justify-center items-center uppercase text-sm">
+          {navItems.map((nav) => {
+            return (
+              <>
+                <a
+                  href={nav.url}
+                  title={nav.title}
+                  className="font-abril hover:text-yellow-500 font-extrabold transition-all duration-200"
+                >
+                  {nav.title}
+                </a>
+              </>
+            );
+          })}
+        </section>
+        <section className="col-span-2 flex justify-end items-center">
+          <button className="px-4 pt-2 pb-1 border-2 border-white hover:border-yellow-500 hover:bg-yellow-500 rounded-full uppercase font-abril text-sm hover:text-black transition-all duration-200">
+            Build with us
+          </button>
+        </section>
       </section>
     </nav>
   );
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,5 @@
 import Navigation from "../Components/Navigation.jsx";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 
 const Welcome = () => {
   const previousWork = [
@@ -17,36 +17,58 @@ const Welcome = () => {
       title: "Intercorp",
       img: "./Previous Work/prev_intercorp.jpg",
     },
-    {
-      title: "Cadeaurable",
-      img: "./Previous Work/prev_cadeaurable.jpg",
-    },
-    {
-      title: "Teacher Speechie",
-      img: "./Previous Work/prev_teacher-speechie.jpg",
-    },
+    // {
+    //   title: "Cadeaurable",
+    //   img: "./Previous Work/prev_cadeaurable.jpg",
+    // },
+    // {
+    //   title: "Teacher Speechie",
+    //   img: "./Previous Work/prev_teacher-speechie.jpg",
+    // },
   ];
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log(window.innerWidth);
+    console.log(document.querySelector(".previous-work-wrapper").offsetWidth);
+  });
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize", () => {
+      document.querySelectorAll(".previous-work-item").forEach((item) => {
+        item.style.width = window.innerWidth / 2 + "px";
+      });
+
+      document.querySelector(".previous-work-wrapper").style.width =
+        previousWork.length * (window.innerWidth / 2 + 16) + "px";
+
+      document.querySelector(".previous-work-wrapper").style.left =
+        -(
+          document.querySelector(".previous-work-wrapper").offsetWidth -
+          window.innerWidth
+        ) /
+          2 +
+        "px";
+    });
+  });
 
   return (
     <div>
       {/* Navigation */}
       <Navigation />
       {/* Hero */}
-      <article className="hero px-14 py-32 flex flex-col justify-center bg-black text-white">
-        <section className="">
-          <h1 className="max-w-2xl font-abril text-6xl font-bold">
+      <article className="hero px-6 lg:px-14 py-32 flex flex-col justify-center bg-black text-white">
+        <section className="mx-auto lg:mx-0 flex flex-col items-center lg:items-start text-center lg:text-left">
+          <h1 className="w-full md:max-w-lg lg:max-w-2xl font-abril text-3xl md:text-4xl lg:text-6xl font-bold">
             Transforming your digital vision into reality
           </h1>
-          <h3 className="max-w-xl text-3xl font-light">
+          <h3 className="max-w-xl text-xl lg:text-3xl font-light">
             We simplify the process with tailored business packages and
             transparent pricing
           </h3>
         </section>
 
-        <section className="mt-24 flex gap-8">
-          <button className="px-4 py-1 bg-white hover:bg-yellow-500 rounded-full uppercase text-black text-lg font-normal transition-all duration-200">
+        <section className="mt-14 lg:mt-24 flex flex-col lg:flex-row gap-8 justify-start items-center">
+          <button className="px-4 py-1 bg-white hover:bg-yellow-500 rounded-full uppercase text-black text-base lg:text-lg font-normal transition-all duration-200">
             Build with us
           </button>
           <button className="uppercase border-b-2 border-white hover:border-yellow-500 hover:text-yellow-500 transition-all duration-200">
@@ -57,8 +79,8 @@ const Welcome = () => {
       {/* Main Content */}
       <main>
         {/* Short Blurb */}
-        <section className="py-14 bg-black text-white text-xl font-light shadow-2xl shadow-black">
-          <article className="mx-auto max-w-xl text-center">
+        <section className="px-6 py-14 bg-black text-white text-lg lg:text-xl font-light shadow-2xl shadow-black">
+          <article className="mx-auto w-full max-w-lg lg:max-w-xl text-center">
             RAVENCI specialise in developing superior websites and mobile apps
             tailored for small to medium-sized businesses. Our mission is to
             transform your digital aspirations into tangible results. If you're
@@ -70,23 +92,33 @@ const Welcome = () => {
         <section className="starry py-28 flex flex-col items-center max-w-full text-white">
           <h2 className="z-10">Previous Work</h2>
 
-          <section className="pt-16 w-full overflow-x-hidden">
+          <section className="relative pt-16 w-full overflow-x-hidden">
             <div
-              className=""
+              className="previous-work-wrapper relative"
               style={{
                 width:
-                  previousWork.length * (window.innerWidth / 2 + 32) + "px",
-                marginLeft: -(window.innerWidth / 4 + 16) + "px",
+                  previousWork.length * (window.innerWidth / 2 + 16) + "px",
+                left:
+                  -(
+                    document.querySelector(".previous-work-wrapper")
+                      .offsetWidth - window.innerWidth
+                  ) /
+                    2 +
+                  "px",
               }}
             >
               {previousWork.map((work) => {
                 return (
                   <article
-                    className="mx-4 my-0 pt-10 pb-0 px-10 inline-block bg-yellow-500"
+                    className="previous-work-item mx-2 my-0 pt-4 md:pt-6 lg:pt-10 px-4 md:px-6 lg:px-10 inline-block bg-yellow-500"
                     style={{ width: window.innerWidth / 2 }}
                     key={work.title}
                   >
-                    <img className="" src={work.img} alt="" />
+                    <img
+                      className="shadow-black shadow-xl"
+                      src={work.img}
+                      alt=""
+                    />
                   </article>
                 );
               })}
