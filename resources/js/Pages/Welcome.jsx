b30fa11c635923f8c6c98c944f7945a1c41d0bc2
richardhaddadau <richard@haddads.net.au>
2023-05-25 17:45:17 +1000

    build: previous work section laid out

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,40 @@
 import Navigation from "../Components/Navigation.jsx";
+import { useEffect } from "react";
 
 const Welcome = () => {
+  const previousWork = [
+    {
+      title: "QRmory",
+      img: "./Previous Work/prev_qrmory.jpg",
+    },
+
+    {
+      title: "Nikita Morell",
+      img: "./Previous Work/prev_nikita-morell.jpg",
+    },
+
+    {
+      title: "Intercorp",
+      img: "./Previous Work/prev_intercorp.jpg",
+    },
+    {
+      title: "Cadeaurable",
+      img: "./Previous Work/prev_cadeaurable.jpg",
+    },
+    {
+      title: "Teacher Speechie",
+      img: "./Previous Work/prev_teacher-speechie.jpg",
+    },
+  ];
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       {/* Navigation */}
       <Navigation />
       {/* Hero */}
-      <article className="px-14 py-32 flex flex-col justify-center bg-black text-white">
+      <article className="hero px-14 py-32 flex flex-col justify-center bg-black text-white">
         <section className="">
           <h1 className="max-w-2xl font-abril text-6xl font-bold">
             Transforming your digital vision into reality
@@ -27,19 +55,53 @@ const Welcome = () => {
         </section>
       </article>
       {/* Main Content */}
-      <main></main>
-      {/* Short Blurb */}
-      <article></article>
-      {/* Previous Work */}
-      <section></section>
-      {/* Testimonial */}
-      <article></article>
-      {/* Packages */}
-      <article>
-        <h2></h2>
-        <h3></h3>
-        <section></section>
-      </article>
+      <main>
+        {/* Short Blurb */}
+        <section className="py-14 bg-black text-white text-xl font-light shadow-2xl shadow-black">
+          <article className="mx-auto max-w-xl text-center">
+            RAVENCI specialise in developing superior websites and mobile apps
+            tailored for small to medium-sized businesses. Our mission is to
+            transform your digital aspirations into tangible results. If you're
+            looking for a robust website or a dynamic app to push your business
+            forward, we are ready to deliver.
+          </article>
+        </section>
+        {/* Previous Work */}
+        <section className="starry py-28 flex flex-col items-center max-w-full text-white">
+          <h2 className="z-10">Previous Work</h2>
+
+          <section className="pt-16 w-full overflow-x-hidden">
+            <div
+              className=""
+              style={{
+                width:
+                  previousWork.length * (window.innerWidth / 2 + 32) + "px",
+                marginLeft: -(window.innerWidth / 4 + 16) + "px",
+              }}
+            >
+              {previousWork.map((work) => {
+                return (
+                  <article
+                    className="mx-4 my-0 pt-10 pb-0 px-10 inline-block bg-yellow-500"
+                    style={{ width: window.innerWidth / 2 }}
+                    key={work.title}
+                  >
+                    <img className="" src={work.img} alt="" />
+                  </article>
+                );
+              })}
+            </div>
+          </section>
+        </section>
+        {/* Testimonial */}
+        <article></article>
+        {/* Packages */}
+        <article>
+          <h2></h2>
+          <h3></h3>
+          <section></section>
+        </article>
+      </main>
       {/* Footer */}
       <footer></footer>
     </div>
