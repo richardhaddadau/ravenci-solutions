d5c8ab5dfffd842c8f83ada22f8ac9c45fb4213f
richardhaddadau <richard@haddads.net.au>
2023-09-09 03:26:22 +1000

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "RAVENCI Group",
@@ -14,6 +11,24 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/vme4ark.css" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="bg-secondary">{children}</body>
     </html>
