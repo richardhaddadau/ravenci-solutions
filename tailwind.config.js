8236505289b93e3ab2adb0b03e0238d55e61f86d
richardhaddadau <richard@haddads.net.au>
2023-09-12 09:41:24 +1000

    move to nextjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./resources/js/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       fontFamily: {
-        abril: ["abril-display", "serif"],
+        serif: ["abril-display", "serif"],
         sans: ["europa", "sans-serif"],
       },
-      minHeight: {
-        hero: "600px",
+      colors: {
+        primary: {
+          DEFAULT: "#d3a62a",
+        },
+        secondary: {
+          DEFAULT: "#212121",
+        },
       },
-      maxWidth: {
-        xxs: "15rem",
+      width: {
+        windowed: "1200px",
       },
     },
   },
